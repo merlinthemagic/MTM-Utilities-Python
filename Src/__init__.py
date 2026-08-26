83ed385d2abd69888bddb4070795332bd49c8276
martin-madsen <martin-madsen@m-u-4>
2026-08-26 18:39:54 +0100

    weg

--- Src/__init__.py
+++ Src/__init__.py
@@ -1,8 +1,4 @@
 
-##Keep byte cache in temp location to avoid disk wear on IoT devices with eeproms or 
-import sys
-sys.pycache_prefix = "/tmp/pycache"
-
 from ._bootstrap import loadEnvVars,loadDependencies
 loadEnvVars(); ##Load environment variables
 loadDependencies(); ##Load Dependencies
